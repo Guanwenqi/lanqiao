e31d88f0c81866caab4ff2e15f2e229c4f79e33a
Maxguan <bszguanwenqi@163.com>
2024-03-08 08:45:04 +0800

    interpretation

--- lesson7/地图.py
+++ lesson7/地图.py
@@ -1,5 +1,5 @@
 # from functools import lru_cache
-# @lru_cache(maxsize=None)
+# @lru_cache(maxsize=None)若这里不用记忆化搜索的库，可以自己手搓四维数组记录结果
 def dfs(x,y,v,depth):
     if depth>k:
         return 0
@@ -21,12 +21,11 @@ dir=[(0,1),(1,0)]
 a=0
 n,m,k=map(int,input().split())
 path=[input() for i in range(n)]
-f=[[[[-1]*6 for i in range(3)]for i in range(m+1)]for i in range(n+1)]
+f=[[[[-1]*6 for i in range(3)]for i in range(m+1)]for i in range(n+1)]#不好直接想象，可以按照维度顺序的逆序依次书写变量，类比二维数组定义方法
 if path[0][1]=='.':
     a+=dfs(0,1,0,0)
 if path[1][0]=='.':
     a+=dfs(1,0,1,0)
 print(a)
-
 
 
